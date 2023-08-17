54fcd19dc26d95d5fe6b48caa6d729b2ae882638
Ariany Mendes <arianymendes@Arianys-MBP.lan>
2023-08-16 22:59:57 -0400

    transactions components , notfound, login and signup form, added a new image , added folder helper and dateand amount format, added usecontext and added css for login ,signup,notfound and general style.css

---
+++ client/vault-react/src/components/Login.js
@@ -0,0 +1,72 @@
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+import "../styles/login.css";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const auth = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:8080/authenticate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    if (response.status === 200) {
+      const { jwt_token } = await response.json();
+      console.log(jwt_token);
+      auth.login(jwt_token);
+      navigate("/");
+    } else if (response.status === 403) {
+      setErrors(["Login failed."]);
+    } else {
+      setErrors(["Unknown error."]);
+    }
+  };
+
+  return (
+    <div className="login-form">
+      <h2>Login</h2>
+      {errors.map((error, i) => (
+        <div key={i}> {error} </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            onChange={(event) => setUsername(event.target.value)}
+            value={username}
+            id="username"
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+            value={password}
+            id="password"
+          />
+        </fieldset>
+        <div className="group-button">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
