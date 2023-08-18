abb044fd308f5a9dd26024b6aba10a33569a8451
DianaNesteruk <dianaanesteruk@gmail.com>
2023-08-17 21:49:59 -0400

    fixed navs and routes

--- client/vault-react/src/components/navbar.js
+++ client/vault-react/src/components/navbar.js
@@ -1,32 +1,36 @@
 import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import { useContext } from "react";
+import "../style/dashboard.css";
+
 
 const Navbar = () => {
   const auth = useContext(AuthContext);
   const user = auth.user;
 
+  const navClassName = user ? "sidebar" : "nav-list";
+
   return (
-    <nav className="nav-list">
+    <nav className={`${navClassName}`}>
+
       {/* always */}
       
       {/* only logged in */}
-      {user && (
+      
+      {user ? (
         <>
-          <Link to="/dashboard">Home</Link>
-          <Link to="/user/:userId/transactions">Transactions</Link>
-          <Link to= "/user/:userId/budgets">Budgets</Link>
-          <Link to="/user/:userId/savings">Savings</Link>
-          <Link to="/user/:userId/reports">Reports</Link>
+          <Link to="/dashboard">Dashboard</Link> {" "}
+          <Link to="/transactions">Transactions</Link> {" "}
+          <Link to="/budgets">Budgets</Link> {" "}
+          <Link to="/savings">Savings</Link> {" "}
+          <Link to="/user/:userId/reports">Reports</Link> {" "}
           <Link onClick={auth.logout}>Logout</Link>
-
         </>
-      )}
-      {/* only logged out */}
-      {!user && (
+      ) : (
         <>
           <Link to="/">Home</Link> {" "}
-          <Link to="/login">Log In</Link> <Link to="/signup">Signup</Link>
+          <Link to="/login">Log In</Link> {" "} 
+          <Link to="/signup">Signup</Link>
         </>
       )}
     </nav>
