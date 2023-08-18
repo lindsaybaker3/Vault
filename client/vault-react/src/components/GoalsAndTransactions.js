b89eb663e681e0ff55cf0cb9a23bdf168d72770f
Lindsay Baker <lindsaynbaker3@gmail.com>
2023-08-18 14:20:38 -0400

    delete goal and styling

--- client/vault-react/src/components/GoalsAndTransactions.js
+++ client/vault-react/src/components/GoalsAndTransactions.js
@@ -2,6 +2,9 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router"
 import AuthContext from "../context/AuthContext";
 import { Link } from "react-router-dom";
+import ConfirmDeleteGoal from "./DeleteGoal";
+import DeleteGoal from "./DeleteGoal";
+import "../style/goalsandtransactions.css"
 
 
 const GoalsAndTransactions = () => {
@@ -45,30 +48,40 @@ useEffect(() => {
 }, [params.goalsId])
 console.log(goal)
 
+const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const handleDeleteClick = () => {
+    setShowDeleteModal(true);
+  };
+
 return (
     <div className = "Goals-details-container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <div className = "goal-section">
             <table>
                 <thead>
                     <tr>
-                        <th className = "goal-header">{goal.categoryName}</th>
+                        <th className = "goal-header" colSpan={"2"}>{goal.categoryName}</th>
+                        <th className="goal-currentbalance">Current Balance: {goal.currentBalance}</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td className = "goal-amount">{goal.amount}</td>
-                        <td className = "goal-startDate">{goal.startDate}</td>
-                        <td className = "goal-endDate">{goal.endDate}</td>
-                        <td className="goal-CurrentBalance">{goal.currentBalance}</td>        
+                    <td className="goal-dates" colSpan="2">
+                            {goal.startDate} - {goal.endDate}
+                    </td>
+                        <td className = "goal-amount">Budget: {goal.amount}</td>
 
+                    </tr>
+                    <tr>
+                        <td colSpan={"3"}>
                         <Link to = { goal.type === "spending" ? `/budgets/edit/${goal.goalsId}` : `/savings/edit/${goal.goalsId}`}>Edit</Link>
-                        <Link to = {`/budgets/delete/${goal.goalsId}`}>Delete</Link>
-
+                        <Link to="#" onClick = {handleDeleteClick}>Delete</Link>
+                        </td>
                     </tr>
                 </tbody>
             </table>
         </div>
-        <div className = "transactions-section">
+        <div className = "transactions-section ">
             {goal?.transactionsList?.map((transaction) => (
                 <div key = {transaction.transactionId} className = "transaction">
                     <span className = "transaction-date">{transaction.transactionDate}</span>
@@ -77,6 +90,15 @@ return (
                 </div>
             ))}
         </div>
+        <div className = "current-balance-line">
+            Total: {goal.currentBalance}
+                    </div>
+        {showDeleteModal && (
+        <DeleteGoal
+          goal={goal} // Pass the goal data to the confirmation modal
+          onClose={() => setShowDeleteModal(false)} // Close modal callback
+        />
+      )}
     </div>
 )
 
