52506ddc871cce17f0c7ddb46778d6c4bb172584
Ariany Mendes <arianymendes@Arianys-MBP.lan>
2023-08-21 01:32:49 -0400

    report front end done

--- client/vault-react/src/components/ReportList.js
+++ client/vault-react/src/components/ReportList.js
@@ -1,6 +1,29 @@
 import React, { useContext, useEffect, useState } from "react";
 import AuthContext from "../context/AuthContext";
 import { Link } from "react-router-dom";
+import { Container } from "@mui/system";
+import LastReport from "../helpers/LastReport";
+
+import DrawerComponent from "./Drawer";
+import {
+  Box,
+  Card,
+  CardContent,
+  CssBaseline,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
+import { Tab } from "@mui/base";
+// ------
 
 const ReportList = () => {
   const auth = useContext(AuthContext);
@@ -24,62 +47,119 @@ const ReportList = () => {
     loadReports();
   }, []);
 
-  console.log(auth, "passou aqui");
-  console.log(auth.user, "passou aqui 2");
+  const downloadFileUrl = (url) => {
+    const fileName = url;
+  };
 
   const getUsernameWithoutDomain = (username) => {
     const parts = username.split("@");
     return parts[0];
   };
 
+  const message = `Hey ${getUsernameWithoutDomain(auth.user.username)
+    .charAt(0)
+    .toUpperCase()}${getUsernameWithoutDomain(auth.user.username).slice(1)}, `;
+
+  const getLastReportUrl = () => {
+    const reportContext = require.context("../reports", false, /\.pdf$/);
+    const reportFiles = reportContext.keys();
+
+    if (reportFiles.length > 0) {
+      const lastReportPath = reportFiles[reportFiles.length - 1];
+      return reportContext(lastReportPath);
+    }
+
+    return null;
+  };
+
+  const lastReportUrl = getLastReportUrl();
+
   return (
-    <div>
-      <p>{`Hey ${getUsernameWithoutDomain(
-        auth.user.username
-      )}, here are your reports:`}</p>
-      <table>
-        <thead>
-          <tr>
-            {/* <th>ID</th>
-            <th>App User ID</th> */}
-            <th>Start Range Date</th>
-            <th>Range End Date</th>
-            <th>Goal Type</th>
-            <th>Report URL</th>
-            <th>Delete?</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reports.map((report) => (
-            <tr key={report.reportId}>
-              {/* <td>{report.reportId}</td>
-              <td>{report.appUserId}</td> */}
-              <td>{report.startDate}</td>
-              <td>{report.endDate}</td>
-              <td>{report.goalType}</td>
-              <td>
-                <a
-                  href={report.reportUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View PDF
-                </a>
-              </td>
-              <td>
-                {auth.user && (
-                  <Link to={`/delete/${report.reportId}`}>Delete</Link>
+    <ThemeProvider theme={createTheme()}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <DrawerComponent />
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                gap: "16px",
+                paddingTop: "64px",
+              }}
+            >
+              <div>
+                {reports.length === 0 ? (
+                  <p>
+                    {message}
+                    <br></br>
+                    No reports available . <br></br>Let's create a report.
+                  </p>
+                ) : (
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Start Range Date</th>
+                        <th>Range End Date</th>
+                        <th>Goal Type</th>
+                        <th>Report URL</th>
+                        <th>Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {reports.map((report) => (
+                        <tr key={report.reportId}>
+                          <td>{report.startDate}</td>
+                          <td>{report.endDate}</td>
+                          <td>{report.goalType}</td>
+                          {/* <td>
+                            <a
+                              href={lastReportUrl} // Use lastReportUrl here
+                              download="Example-PDF-document"
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <button>Download.pdf file</button>
+                            </a>
+                        
+                          </td> */}
+                          <td>
+                            <LastReport />{" "}
+                            {/* Place LastReport component here */}
+                          </td>
+                          <td>
+                            {auth.user && (
+                              <Link to={`/delete/${report.reportId}`}>
+                                Delete
+                              </Link>
+                            )}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
                 )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button>
-        {" "}
-        <Link to="/report/add">Create Report</Link>
-      </button>
-    </div>
+                <Link to="/report/add">
+                  <button>&nbsp;&nbsp;Create Report</button>
+                </Link>
+              </div>
+            </Box>
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 };
 
--- client/vault-react/src/components/TransactionsList.js
+++ client/vault-react/src/components/TransactionsList.js
@@ -3,12 +3,28 @@ import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import AmountDisplay from "../helpers/AmountDisplay";
 import FormattedDate from "../helpers/FormattedDate";
-import {  Container } from "@mui/system";
+import { Container } from "@mui/system";
 import DrawerComponent from "./Drawer";
-import { Box, Button, Card, CardContent, CssBaseline, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, ThemeProvider, Typography, createTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CssBaseline,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
 import { Tab } from "@mui/base";
 // ------
-
 
 const TransactionsList = () => {
   const auth = useContext(AuthContext);
@@ -34,81 +50,79 @@ const TransactionsList = () => {
   }, []);
 
   return (
-    <ThemeProvider theme = {createTheme()}>
-    <Box sx={{ display: 'flex' }}>
+    <ThemeProvider theme={createTheme()}>
+      <Box sx={{ display: "flex" }}>
         <CssBaseline />
         <DrawerComponent />
-    <Box
-      component="main"
-      sx={{
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'light'
-            ? theme.palette.grey[100]
-            : theme.palette.grey[900],
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-      }}
-    >
-    <Container maxWidth = "lg" sx={{ mt: 4, mb: 4 }}>
-    <Box 
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        gap: '16px', // Adjust the gap between cards
-        paddingTop: '64px',
-      }}>
-      <TableContainer component = {Paper}>
-        <Table sx = {{ minWidth: 650}} aria-label = "simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                Date
-              </TableCell>
-              <TableCell>
-                Goal
-              </TableCell>
-              <TableCell>
-                Description
-              </TableCell>
-              <TableCell>
-                Amount
-              </TableCell>
-              <TableCell>
-                Edit
-              </TableCell>
-              <TableCell>
-                Delete
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {transactions?.map((transaction) => (
-              <TableRow key = {transaction.transactionId}>
-                <TableCell>{transaction.transactionDate}</TableCell>
-                <TableCell>{transaction.category}</TableCell>
-                <TableCell>{transaction.description}</TableCell>
-                <TableCell><AmountDisplay amount={transaction.amount} /> </TableCell>
-                <TableCell>  {auth.user && (
-                  <Link to={`/edit/${transaction.transactionId}`}>Edit</Link>
-                )}</TableCell>
-                <TableCell>
-                {auth.user && (
-                  <Link to={`/delete/${transaction.transactionId}`}>
-                    Delete
-                  </Link>
-                )}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        </TableContainer>
-      <button>Add Transaction</button>
-    </Box>
-    </Container>
-    </Box>
-    </Box>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                gap: "16px", // Adjust the gap between cards
+                paddingTop: "64px",
+              }}
+            >
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Date</TableCell>
+                      <TableCell>Goal</TableCell>
+                      <TableCell>Description</TableCell>
+                      <TableCell>Amount</TableCell>
+                      <TableCell>Edit</TableCell>
+                      <TableCell>Delete</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {transactions?.map((transaction) => (
+                      <TableRow key={transaction.transactionId}>
+                        <TableCell>{transaction.transactionDate}</TableCell>
+                        <TableCell>{transaction.category}</TableCell>
+                        <TableCell>{transaction.description}</TableCell>
+                        <TableCell>
+                          <AmountDisplay amount={transaction.amount} />{" "}
+                        </TableCell>
+                        <TableCell>
+                          {" "}
+                          {auth.user && (
+                            <Link to={`/edit/${transaction.transactionId}`}>
+                              Edit
+                            </Link>
+                          )}
+                        </TableCell>
+                        <TableCell>
+                          {auth.user && (
+                            <Link to={`/delete/${transaction.transactionId}`}>
+                              Delete
+                            </Link>
+                          )}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <Link to="/transaction/add">
+                <button>Add Transaction</button>
+              </Link>
+            </Box>
+          </Container>
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 };
