2f9d000fd4bd9f2d71ac8fb116d6c144a96212a2
Lindsay Baker <lindsaynbaker3@gmail.com>
2023-08-22 12:52:46 -0400

    functionality changes

--- client/vault-react/src/components/ReportList.js
+++ client/vault-react/src/components/ReportList.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 import AuthContext from "../context/AuthContext";
 import { Link } from "react-router-dom";
 import { Container } from "@mui/system";
-import LastReport from "../helpers/LastReport";
+
 import AWS from "aws-sdk";
 import DrawerComponent from "./Drawer";
 import {
