2e200b8c02cfc3f3414f204d927aaf438bbc3a0d
DianaNesteruk <dianaanesteruk@gmail.com>
2023-08-24 16:16:30 -0400

    cleanup first 7 components

--- client/vault-react/src/components/DeleteGoal.js
+++ client/vault-react/src/components/DeleteGoal.js
@@ -14,127 +14,119 @@ function DeleteGoal() {
   const auth = useContext(AuthContext);
   const [goal, setGoal] = useState("");
  
-  
-  //TODO: may need to add more pieces of state but this is what i have right now
-
   const fetchGoal = () => {
     fetch(`http://localhost:8080/api/vault/goals/${params.goalsId}`,
     {headers: {
         Authorization: "Bearer " + auth.user.token 
     },
-})
-.then((response) => {
-    if(response.ok){
-        return response.json();
-    } else {
-        return Promise.reject("No Goal Exists");
-    }
-})
-.then((setGoal))
-.catch((error) => {
-    setErrors([error])
-})
-    
+  })
+  .then((response) => {
+      if(response.ok){
+          return response.json();
+      } else {
+          return Promise.reject("No Goal Exists");
+      }
+  })
+  .then((setGoal))
+  .catch((error) => {
+      setErrors([error])
+  })
 }
-
-
 
 useEffect(() => {
 if(!auth?.user?.token){
     return;
-}else {
+  }else {
     fetchGoal()
-}
+  }
 }, [params.goalsId])
- 
 
-    const handleDelete = () => {
-        fetch(`http://localhost:8080/api/vault/goal/${params.goalsId}`, {
-          method: "DELETE",
-          headers: {
-            Authorization: "Bearer " + auth.user.token,
-          },
-        }).then((response) => {
-          if (response.ok) {
-            navigate("/budgets");
-          } else {
-            console.log(`Unexpected response status code: ${response.status}`);
-          }
-        });
-      };
-    
-      if (goal === null) {
-        return null;
+const handleDelete = () => {
+    fetch(`http://localhost:8080/api/vault/goal/${params.goalsId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + auth.user.token,
+      },
+    }).then((response) => {
+      if (response.ok) {
+        navigate("/budgets");
+      } else {
+        console.log(`Unexpected response status code: ${response.status}`);
       }
+    });
+  };
 
-      const getUsernameWithoutDomain = (username) => {
-        const parts = username.split("@");
-        return parts[0];
-      };
-    
-      const message = `Hey ${getUsernameWithoutDomain(auth.user.username)
-        .charAt(0)
-        .toUpperCase()}${getUsernameWithoutDomain(auth.user.username).slice(1)}, `;
+  if (goal === null) {
+    return null;
+  }
+
+  const getUsernameWithoutDomain = (username) => {
+    const parts = username.split("@");
+    return parts[0];
+  };
+
+  const message = `Hey ${getUsernameWithoutDomain(auth.user.username)
+    .charAt(0)
+    .toUpperCase()}${getUsernameWithoutDomain(auth.user.username).slice(1)}, `;
 
 
-      return (
-        <Box 
-        sx = {{
-          position: 'absolute',
-          top: '50%',
-          left: '55%',
-          transform: 'translate(-50%, -50%)',
-          width: 400,
-          bgcolor: 'background.paper',
-          border: '2px solid #000',
-          boxShadow: 24,
-          p: 4,
-          
-        }}>
-        <div className="modal-content">
-          <h2 style={{ marginBottom: "20px" }}>Delete this Goal?</h2>
-          <p>
-              <strong> {message}</strong>
-           </p>
-          <p>Are you sure you want to delete this goal?</p>
-          <p><strong>Amount:</strong> <AmountDisplay amount = {goal.amount} ></AmountDisplay></p>
-          <p><strong>Start Date: </strong> <FormattedDate date = {goal.startDate} ></FormattedDate></p>
-          <p><strong>End Date: </strong> <FormattedDate date = {goal.endDate} ></FormattedDate></p>
+  return (
+      <Box 
+      sx = {{
+        position: 'absolute',
+        top: '50%',
+        left: '55%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+        
+      }}>
+      <div className="modal-content">
+        <h2 style={{ marginBottom: "20px" }}>Delete this Goal?</h2>
+        <p>
+          <strong> {message}</strong>
+        </p>
+        <p>Are you sure you want to delete this goal?</p>
+        <p><strong>Amount:</strong> <AmountDisplay amount = {goal.amount} ></AmountDisplay></p>
+        <p><strong>Start Date: </strong> <FormattedDate date = {goal.startDate} ></FormattedDate></p>
+        <p><strong>End Date: </strong> <FormattedDate date = {goal.endDate} ></FormattedDate></p>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{
+            backgroundColor: "red",
+            color: "#FFFFFF",
+            "&:hover": {
+              backgroundColor: "#69b45E",
+            },
+            marginLeft: "65px",
+            marginRight: '10px'
+          }}
+          onClick={handleDelete}
+        >
+          Delete
+        </Button>
+        <Link to={goal.type === "spending" ? "/budgets" : "/savings"}>
           <Button
-                  variant="contained"
-                  color="primary"
-                  sx={{
-                    backgroundColor: "red",
-                    color: "#FFFFFF",
-                    "&:hover": {
-                      backgroundColor: "#69b45E",
-                    },
-                    marginLeft: "65px",
-                    marginRight: '10px'
-                  }}
-                  onClick={handleDelete}
-                >
-                  Delete
-            </Button>
-          <Link to={goal.type === "spending" ? "/budgets" : "/savings"}>
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: "#05391F",
-                color: "#FFFFFF",
-                "&:hover": {
-                  backgroundColor: "#69B45E",
-                },
-              }}
-            >
-              Cancel
-            </Button>
-          </Link>
-        </div>
-      </Box>
-
-      );
-    }
+            variant="contained"
+            sx={{
+              backgroundColor: "#05391F",
+              color: "#FFFFFF",
+              "&:hover": {
+                backgroundColor: "#69B45E",
+              },
+            }}
+          >
+            Cancel
+          </Button>
+        </Link>
+      </div>
+    </Box>
+  );
+}
     
 export default DeleteGoal;
 
